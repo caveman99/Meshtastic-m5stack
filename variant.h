a837fd3cd603f32f9134d84ccc7a780ac86752da
Thomas Göttgens <tgoettgens@gmail.com>
2022-05-06 16:55:08 +0200

    Clean up Variant file

--- variant.h
+++ variant.h
@@ -1,17 +1,9 @@
-// #define BUTTON_NEED_PULLUP // if set we need to turn on the internal CPU pullup during sleep
-
 #define I2C_SDA 21
 #define I2C_SCL 22
 
-// #define BUTTON_PIN 39 // 38, 37
-//#define BUTTON_PIN 0
 #define BUTTON_NEED_PULLUP
-// #define EXT_NOTIFY_OUT 13 // Default pin to use for Ext Notify Plugin.
-
-#define UPDOWN1_PIN_UP 39
-#define UPDOWN1_PIN_DOWN 37
 #define BUTTON_PIN 38
-#define UPDOWN1_PIN_ESC 0 // old user button
+
 
 #undef RF95_SCK
 #undef RF95_MISO
@@ -24,15 +16,13 @@
 #define RF95_NSS 5
 
 #define USE_RF95
-#define LORA_DIO0 36 // a No connect on the SX1262 module
+#define LORA_DIO0 36
 #define LORA_RESET 26
-#define LORA_DIO1 RADIOLIB_NC// Not really used
-#define LORA_DIO2 RADIOLIB_NC// Not really used
+#define LORA_DIO1 RADIOLIB_NC
+#define LORA_DIO2 RADIOLIB_NC
 
-// This board has different GPS pins than all other boards
+// This board has no GPS or Screen for now
 #undef GPS_RX_PIN
 #undef GPS_TX_PIN
-#define GPS_RX_PIN 16
-#define GPS_TX_PIN 17
-
 #define NO_GPS
+#define NO_SCREEN
